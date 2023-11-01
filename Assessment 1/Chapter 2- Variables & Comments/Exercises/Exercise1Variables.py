258601efe82a0912bfefed23e43118c47e6efb84
khaetlie <144931479+khaetlie@users.noreply.github.com>
2023-11-01 21:17:17 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise1Variables.py
@@ -0,0 +1,10 @@
+variable = "Happy Birthday!"
+print(variable)
+
+variable = "Happy New Year!"
+print(variable)
+
+x = 50 #This is the amount of pounds she has
+y = 6 #This is the price of USB sticks
+z = 50/6
+print(z)
