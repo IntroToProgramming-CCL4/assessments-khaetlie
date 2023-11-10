37bba35c1f5e96247132cb8bdc4d92a85ba16779
khaetlie <144931479+khaetlie@users.noreply.github.com>
2023-11-11 03:19:50 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/Exercise 1 Person.py
@@ -0,0 +1,5 @@
+person = {'first_name': 'Allyzah', 'last_name': 'Acuna', 'age': 6, 'city':'Dubai' }
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
